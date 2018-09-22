fbdd17c836b470b59d5a650111ee08d7eecb80d6
Bill St. Clair <billstclair@gmail.com>
2018-09-22 19:50:34 -0400

    Geolocation.js works in the Chrome DevTools console with elm.js.dummy.

--- example/site/js/PortFunnel/Geolocation.js
+++ example/site/js/PortFunnel/Geolocation.js
@@ -1,110 +1,145 @@
-/*
+//////////////////////////////////////////////////////////////////////
+//
+// Geolocation.js
+// JavaScript runtime code for Elm PortFunnel.Geolocation module.
+// Copyright (c) 2018 Bill St. Clair <billstclair@gmail.com>
+// Portions Copyright (c) 2015-2016, Evan Czaplicki
+// All rights reserved.
+// Distributed under the MIT License
+// See LICENSE
+//
+//////////////////////////////////////////////////////////////////////
 
-import Elm.Kernel.Scheduler exposing (binding, succeed, fail, rawSpawn)
-import Maybe exposing (Maybe(Just,Nothing))
+(function() {
+  var moduleName = 'Geolocation';
+  var sub = PortFunnel.sub;
+  if (!sub) {
+    return;
+  }
 
-*/
+  PortFunnel.modules[moduleName].cmd = dispatcher;
+
+  // Let the Elm code know we've started
+  sub.send({ module: moduleName,
+             tag: "startup",
+             args : null
+           });
+
+  var tagTable =
+      { getlocation: getLocation,
+        sendchanges: sendChanges,
+        stopchanges: stopChanges
+      }
+
+  function dispatcher(tag, args) {
+    let f = tagTable[tag];
+    if (f) {
+      return f(args);
+    }
+  }
+
+  function sendObject(tag, args) {
+    sub.send({ moduleName: moduleName,
+               tag: tag,
+               args: args
+             });
+  }
+
+  function sendPosition(rawPosition) {
+    var location = encodeLocation(rawPosition);
+    sendObject("location", location);
+  }
+
+  function getLocation(args) {
+    function onError(rawError) {
+      var err = encodeError(rawError);
+      sendObject("error", err);
+    }
+    var options = args;
+    navigator.geolocation.getCurrentPosition(
+      sendPosition, onError, rawOptions(options));
+  }
+
+  var watching = false;
+  var watchid = null;
+
+  function sendChanges(args) {
+    if (!watching) {
+      watchid = navigator.geolocation.watchPosition(sendPosition);
+      watching = true;
+    }
+  }
+
+  function stopChanges(args) {
+    if (watching) {
+      watching = false;
+      navigator.geolocation.clearWatch(watchid);
+    }
+  }
 
 
+  // OPTIONS
 
-// LOCATIONS
+  var defaultOptions =
+      { enableHighAccuracy: false,
+        timeout: undefined,
+        maximumAge: 0
+      }
 
-function _Geolocation_toLocation(rawPosition)
-{
+  function rawOptions(options) {
+    if (!options) {
+      // For debugging. The Elm code always passes options.
+      return defaultOptions;
+    } else {
+	  return { enableHighAccuracy: options.enableHighAccuracy,
+	           timeout: options.timeout || undefined,
+	           maximumAge: options.maximumAge || 0
+	         };
+    }
+  }
+
+
+  // LOCATIONS
+
+  function encodeLocation(rawPosition) {
 	var coords = rawPosition.coords;
 
 	var rawAltitude = coords.altitude;
 	var rawAccuracy = coords.altitudeAccuracy;
 	var altitude =
 		(rawAltitude === null || rawAccuracy === null)
-			? __Maybe_Nothing
-			: __Maybe_Just({ __$value: rawAltitude, __$accuracy: rawAccuracy });
-
+		? null
+		: { value: rawAltitude,
+            accuracy: rawAccuracy
+          };
 	var heading = coords.heading;
 	var speed = coords.speed;
 	var movement =
 		(heading === null || speed === null)
-			? __Maybe_Nothing
-			: __Maybe_Just(
-				speed === 0
-					? { $: 'Static' }
-					: { $: 'Moving', a: { __$speed: speed, __$degreesFromNorth: heading } }
-			);
-
-	return {
-		__$latitude: coords.latitude,
-		__$longitude: coords.longitude,
-		__$accuracy: coords.accuracy,
-		__$altitude: altitude,
-		__$movement: movement,
-		__$timestamp: rawPosition.timestamp
-	};
-}
+		? null
+		: (speed === 0
+		   ? 'static'
+	       : { speed: speed, degreesFromNorth: heading }
+          );
+  	return { latitude: coords.latitude,
+		     longitude: coords.longitude,
+		     accuracy: coords.accuracy,
+		     altitude: altitude,
+		     movement: movement,
+		     timestamp: rawPosition.timestamp
+	       };
+  }
 
 
-// ERRORS
+  // ERRORS
 
-var _Geolocation_errorTypes = ['PermissionDenied', 'PositionUnavailable', 'Timeout'];
+  var errorTypes = ['PermissionDenied', 'PositionUnavailable', 'Timeout'];
 
-function _Geolocation_toError(rawError)
-{
-	return {
-		$: _Geolocation_errorTypes[rawError.code - 1],
-		a: rawError.message
-	};
-}
+  function encodeError(rawError) {
+    var key = errorTypes[rawError.code - 1];
+    var res = {};
+    res[key] = rawError.message
+    return res;
+  }
 
-
-// OPTIONS
-
-function _Geolocation_fromOptions(options)
-{
-	return {
-		enableHighAccuracy: options.__$enableHighAccuracy,
-		timeout: options.__$timeout.a,
-		maximumAge: options.__$maximumAge.a || 0
-	};
-}
-
-
-// GET LOCATION
-
-function _Geolocation_now(options)
-{
-	return __Scheduler_binding(function(callback)
-	{
-		function onSuccess(rawPosition)
-		{
-			callback(__Scheduler_succeed(_Geolocation_toLocation(rawPosition)));
-		}
-
-		function onError(rawError)
-		{
-			callback(__Scheduler_fail(_Geolocation_toError(rawError)));
-		}
-
-		navigator.geolocation.getCurrentPosition(onSuccess, onError, _Geolocation_fromOptions(options));
-	});
-}
-
-function _Geolocation_watch(options, toSuccessTask, toErrorTask)
-{
-	return __Scheduler_binding(function(callback)
-	{
-		function onSuccess(rawPosition)
-		{
-			__Scheduler_rawSpawn(toSuccessTask(_Geolocation_toLocation(rawPosition)));
-		}
-
-		function onError(rawError)
-		{
-			__Scheduler_rawSpawn(toErrorTask(_Geolocation_toError(rawError)));
-		}
-
-		var id = navigator.geolocation.watchPosition(onSuccess, onError, _Geolocation_fromOptions(options));
-
-		return function() {
-			navigator.geolocation.clearWatch(id);
-		};
-	});
-}
+})();
